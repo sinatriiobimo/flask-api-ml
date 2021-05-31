7f1848ca30c5d81d9cd92392f1f5379704b74b7e
sinatriiobimo <sinatriobimbim@gmail.com>
2021-05-31 20:36:56 +0700

    'final'

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from model.model import Model
+import os
 import json
 
 app = Flask(__name__)
@@ -17,3 +18,6 @@ def predict():
     prediction= model.predict(request_json.get('image'))
 
     return jsonify(prediction)
+
+if __name__ == "__main__":
+  app.run(debug=True, host='0.0.0.0', port='80')
